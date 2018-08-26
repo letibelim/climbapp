6c083a44e3295af0f057f48e450e91bc192e6be1
Thibault Le Grand <thibault.legrand@gmail.com>
2018-08-26 17:30:50 +0200

    create Site entity

---
+++ api/src/Entity/Site.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thibault
+ * Date: 26/08/18
+ * Time: 16:46
+ */
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * This is a dummy entity. Remove it!
+ *
+ * @ApiResource
+ * @ORM\Entity(repositoryClass="SiteRepository")
+ */
+class Site
+{
+    /**
+     * @var int The entity Id
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=512)
+     */
+    private $name;
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=128, nullable=true)
+     */
+    private $coordinates;
+
+    /**
+     * @var bool
+     * @ORM\Column(type="boolean")
+     */
+    private $isKidFriendly = false;
+
+    /**
+     * @var string
+     * @ORM\Column(type="text", nullable=true)
+     */
+    public $description;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Site
+     */
+    public function setName(string $name): Site
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCoordinates(): string
+    {
+        return $this->coordinates;
+    }
+
+    /**
+     * @param string $coordinates
+     *
+     * @return Site
+     */
+    public function setCoordinates(string $coordinates): Site
+    {
+        $this->coordinates = $coordinates;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isKidFriendly(): bool
+    {
+        return $this->isKidFriendly;
+    }
+
+    /**
+     * @param bool $isKidFriendly
+     *
+     * @return Site
+     */
+    public function setIsKidFriendly(bool $isKidFriendly): Site
+    {
+        $this->isKidFriendly = $isKidFriendly;
+        return $this;
+    }
+
+}
