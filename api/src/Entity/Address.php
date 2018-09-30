4a5c554ed4525cf234c4177fc610a98a6ab278da
Thibault Le Grand <thibault.legrand@gmail.com>
2018-09-30 20:11:33 +0200

    Address

---
+++ api/src/Entity/Address.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $line1;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $line2;
+
+    /**
+     * @ORM\Column(type="string", length=16)
+     */
+    private $postcode;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $town;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $country;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Site", mappedBy="address", cascade={"persist", "remove"})
+     */
+    private $site;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLine1(): ?string
+    {
+        return $this->line1;
+    }
+
+    public function setLine1(?string $line1): self
+    {
+        $this->line1 = $line1;
+
+        return $this;
+    }
+
+    public function getLine2(): ?string
+    {
+        return $this->line2;
+    }
+
+    public function setLine2(?string $line2): self
+    {
+        $this->line2 = $line2;
+
+        return $this;
+    }
+
+    public function getPostcode(): ?string
+    {
+        return $this->postcode;
+    }
+
+    public function setPostcode(string $postcode): self
+    {
+        $this->postcode = $postcode;
+
+        return $this;
+    }
+
+    public function getTown(): ?string
+    {
+        return $this->town;
+    }
+
+    public function setTown(string $town): self
+    {
+        $this->town = $town;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getSite(): ?Site
+    {
+        return $this->site;
+    }
+
+    public function setSite(Site $site): self
+    {
+        $this->site = $site;
+
+        // set the owning side of the relation if necessary
+        if ($this !== $site->getAddress()) {
+            $site->setAddress($this);
+        }
+
+        return $this;
+    }
+}
