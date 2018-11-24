0f8c543e775f2bf6477a08bf8859681ca8a4fa3f
Thibault Le Grand <thibault.legrand@gmail.com>
2018-11-24 15:31:20 +0100

    [Bundle] add framework-extra-bundle

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
---
+++ api/src/Form/MediaObjectType.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thibault
+ * Date: 11/11/18
+ * Time: 10:32
+ */
+
+namespace App\Controller;
+
+class MediaObjectType
+{
+
+}
